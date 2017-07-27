f1d3c19138cb47ac895ada8f6e3683759f4dafcf
Felix M <fe_vik@yahoo.de>
2017-07-27 10:09:56 +0200

    Stable Branch aufgeräumt
    -> Sensorknoten projekt entfernt(war der vorläufer zu windregenlicht)
    -> USARTV1 projekt entfernt(war das testprojekt für die usart schnittstelle zum GSM-Modul)

--- Sensorknoten/Versuch1/Versuch1/clock/clock.c
+++
@@ -1,9 +0,0 @@
-#include "clock.h"
-
-void clock_init(void)
-{
-	OSC.CTRL |= OSC_RC2MEN_bm;
-	while(!(OSC.STATUS & OSC_RC2MRDY_bm));
-	CCP = CCP_IOREG_gc;
-	CLK.CTRL = CLK_SCLKSEL_RC2M_gc;
-}
--- Sensorknoten/Versuch1/Versuch1/clock/clock.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLOCK_H
-#define CLOCK_H
-
-#include <avr/io.h>
-
-void clock_init(void);
-
-#endif
